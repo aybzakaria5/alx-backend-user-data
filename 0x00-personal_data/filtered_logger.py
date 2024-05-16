d0fccb0355948cfba8dd117d26236c49f17a4b81
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-16 18:25:35 +0000

    fixing some checks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,7 +2,11 @@
 """filtering loggs"""
 import re
 
-def filter_datum(fields, redaction, message, separator):
-    """filtering from a message """
-    return re.sub(fr'((?:^|{re.escape(separator)})(?:{"|".join(fields)})=)[^{separator}]*(?={re.escape(separator)}|$)', fr'\1{redaction}', message)
 
+def filter_datum(fields, redaction, message, separator):
+    """filteing data and handeling redactions"""
+    pattern = (
+        fr'((?:^|{re.escape(separator)})(?:{"|".join(fields)})=)'
+        fr'[^{separator}]*(?={re.escape(separator)}|$)'
+    )
+    return re.sub(pattern, fr'\1{redaction}', message)
