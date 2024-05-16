a582cfed28f2da2f44fb7bc4e4f3ae5cbe4e8ef6
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-16 18:36:54 +0000

    fixing pycodestyle

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """filtering loggs"""
 import re
-from typing  import List
+from typing import List
+
 
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
