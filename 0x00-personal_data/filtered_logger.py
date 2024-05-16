f5df0d111fd2838474f749de185a415faf1c582d
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-16 18:35:19 +0000

    adding anootations to the functions

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """filtering loggs"""
 import re
+from typing  import List
 
-
-def filter_datum(fields, redaction, message, separator):
-    """filteing data and handeling redactions"""
+def filter_datum(
+    fields: List[str], redaction: str, message: str, separator: str
+) -> str:
+    """filteing data and hiandeling redactions"""
     for field in fields:
         reg = f"{field}=[^{separator}]*"
         message = re.sub(reg, f"{field}={redaction}", message)
