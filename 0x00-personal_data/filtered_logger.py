68927d9cb248f8edcaabccbac02f04fb1fb4a030
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-16 22:05:16 +0000

    connecting the db

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,8 @@
 import re
 from typing import List
 import logging
+import os
+import mysql.connector
 
 
 def filter_datum(
@@ -50,3 +52,21 @@ def get_logger() -> logging.Logger:
 
 
 PII_FIELDS = ('name', 'email', 'phone', 'address', 'credit_card_number')
+
+
+def get_db():
+    """Return a connector to the database
+    (mysql.connector.connection.MySQLConnection object).
+    """
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    db = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database
+    )
+    return db
