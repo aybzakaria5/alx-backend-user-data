6fe8af62784923a25162404c9a58cc3e26aa1f3b
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-16 22:00:58 +0000

    getting loggers

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,3 +33,20 @@ class RedactingFormatter(logging.Formatter):
         msg = filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
         record.msg = msg
         return super().format(record)
+
+
+def get_logger() -> logging.Logger:
+    """Return a logging.Logger object named 'user_data'."""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler = StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+PII_FIELDS = ('name', 'email', 'phone', 'address', 'credit_card_number')
