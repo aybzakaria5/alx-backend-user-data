0b8cc3c5323d7817f6c04121c826a09302d7c362
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-22 17:12:03 +0000

    adding simple authorization

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,12 @@ def not_found(error) -> str:
     return jsonify({"error": "Not found"}), 404
 
 
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """unothorized endpint
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
