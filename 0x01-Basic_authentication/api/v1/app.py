3513ff1257dd326bbababfefaa7e2ab816d54942
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-22 17:14:27 +0000

    fixing pycodestyle

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -27,6 +27,7 @@ def unauthorized(error) -> str:
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
