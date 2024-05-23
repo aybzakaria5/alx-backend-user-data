2296b036f9ff4f002569b99aeb4649759752289e
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-23 17:10:42 +0000

    forbidden

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,6 +28,13 @@ def unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errohandler(403)
+def forbidden(error) -> str:
+    """ forbiden endoiint
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -32,3 +32,11 @@ def unauthorized() -> str:
     Raise a 401 error
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Raise a 403 error
+    """
+    abort(403)
