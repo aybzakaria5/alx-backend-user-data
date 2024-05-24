a09a2544b01e1f9388732066b3ec1f5363a16d13
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-24 23:27:23 +0000

    task 4

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -18,7 +18,19 @@ class Auth:
         are excluded from authentication
         :return: False
         """
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+
+        if not path.endswith('/'):
+            path + = '/'
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/') and excluded_path == path:
+                return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
