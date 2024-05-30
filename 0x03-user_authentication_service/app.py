3e5a14ca8117b5a496ef9f0cdbe931a26458ae85
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-30 15:55:56 +0000

    regestring and checking if regestered

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,14 +1,27 @@
 #!/usr/bin/env python3
 """a basic flask app"""
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, Response, request
+from auth import Auth
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
 def welcome():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def users() -> Response:
+    """regestring users """
+    email = request.form["email"]
+    password = request.form["password"]
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
