e9091ee1f441fd2519c207ee84fbb74f6be34667
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-30 12:45:05 +0000

    truning off printing to the console sqlalchemy statments

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -16,7 +16,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
